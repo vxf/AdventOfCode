d993dc3efc840fcee5a359e1a8827b50bf411c85
Vasco Flores <vasco.flores@gmail.com>
2017-12-08 02:40:59 +0000

    more "tight" version

--- 2017/ch2/ch2_2.erl
+++ 2017/ch2/ch2_2.erl
@@ -1,16 +1,20 @@
 -module(ch2_2).
 -export([main/0, main/1]).
 
-find_div(L) ->
-  [H | T] = lists:sort(fun(A, B) -> B < A end, L),
-  find_div([H | T], T).
+%% find_div(L) ->
+%%   [H | T] = lists:sort(fun(A, B) -> B < A end, L),
+%%   find_div([H | T], T).
  
-find_div([_ | [H | T]], []) ->
-  find_div([H | T], T);
-find_div([Ha | _], [Hb | _]) when (Ha rem Hb) =:= 0 ->
-  Ha div Hb;
-find_div([Ha | Ta], [_ | Tb]) ->
-  find_div([Ha | Ta], Tb).
+%% find_div([_ | [H | T]], []) ->
+%%   find_div([H | T], T);
+%% find_div([Ha | _], [Hb | _]) when (Ha rem Hb) =:= 0 ->
+%%   Ha div Hb;
+%% find_div([Ha | Ta], [_ | Tb]) ->
+%%   find_div([Ha | Ta], Tb).
+
+find_div(L) ->
+  [D] = [A div B || A <- L, B <- L, A > B, (A rem B) =:= 0],
+  D.
 
 solve(Sheet) ->
   lists:sum([find_div(L) || L <- Sheet]).
