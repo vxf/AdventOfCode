3249420f1ff029612ebd3c5315f703f7d0843d3b
Vasco Flores <vasco.flores@gmail.com>
2017-12-08 03:16:21 +0000

    ch3 even TIGHTER

--- 2017/ch3/ch3.py
+++ 2017/ch3/ch3.py
@@ -3,34 +3,22 @@
 
 # vxf
 
+import math
+
 n = 368078
 # n = 1024
 # n = 12
 # n = 23
 
-def odds():
-  x = 0
-  while True :
-    yield x, 2*x + 1
-    x += 1
+# the squares always have an odd side length so we want to search only odd
+# squares 2n+1
+# we want to find the b right above n where (2b+1)**2 > n
+b = math.floor((math.sqrt(n)-1)/2)
 
-a = 0
-b = 0
-c = 0
-for i, x in odds():
-  c = x
-  if x*x > n :
-    break
-  b = i
-  a = x
- 
-#print(a)
-#print(b)
-#print(c)
-# print(((n - a*a) % ((c*c - a*a) // 4)) )
-# print(c*c - a*a)
+# and we also want to find the generating odd number
+a = 2*b + 1
 
+# we calculate the side length
 m = 2 * (b + 1)
-#print(b)
 
 print(b + 1 + abs((n - (a*a)) % m - (b+1)))
