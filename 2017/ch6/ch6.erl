875e2e172e32b6cde30069075e2d45f224c662a3
Vasco Flores <vasco.flores@gmail.com>
2017-12-13 04:20:05 +0000

    ch6 part 1 in Erlang

---
+++ 2017/ch6/ch6.erl
@@ -0,0 +1,55 @@
+-module(ch6).
+-export([main/0, main/1]).
+
+dist(M) ->
+  {P, Max} = maxp(M),
+  dist(M, P, Max, length(M), 0). 
+
+dist([], _, _, _, _) ->
+  [];
+dist([_ | T], P, Max, L, C) when C == P ->
+  [        (Max div L) | dist(T, P, Max, L, C+1)];
+dist([H | T], P, Max, L, C) when C < P, C + L =< P + (Max rem L) ->
+  [H + 1 + (Max div L) | dist(T, P, Max, L, C+1)];
+dist([H | T], P, Max, L, C) when C > P, C =< P + (Max rem L) ->
+  [H + 1 + (Max div L) | dist(T, P, Max, L, C+1)];
+dist([H | T], P, Max, L, C) ->
+  [H +     (Max div L) | dist(T, P, Max, L, C+1)].
+
+solve(M) ->
+  solve([M], 0, false).
+
+solve(_, C, true) ->
+  C;
+solve([M | T], C, false) ->
+  N = dist(M),
+  % io:write(N),
+  % io:fwrite("~n"),
+  solve([N | [M | T]], C+1, lists:member(N, [M | T])).
+
+%% max function that also returns the position on the list, 0-based
+maxp(M) ->
+  maxp(M, 0, -1, -1).
+maxp([], _, P, N) ->
+  {P, N};
+maxp([H | T], C, _, N) when H > N ->
+  maxp(T, C+1, C, H);
+maxp([_ | T], C, P, N) ->
+  maxp(T, C+1, P, N).
+
+%% translates a binary ascii number into integer
+bin_to_integer(B) ->
+  {I, _} = string:to_integer(binary:bin_to_list(B)),
+  I.
+
+main() ->
+  % solve([0,2,7,0]).
+  % solve([11,11,13,7,0,15,5,5,4,4,1,1,7,1,15,11]).
+  % dist([11,11,13,7,0,15,5,5,4,4,1,1,7,1,15,11]).
+  %dist([1,0,2,12,5,4,10,10,9,9,6,6,12,6,4,15]).
+  {ok, Binary} = file:read_file("input.txt"),
+  Sheet = [bin_to_integer(X) || X <- binary:split(Binary, <<"\t">>,[global]), X =/= <<>>],
+  main(Sheet).
+
+main(Sheet) ->
+  solve(Sheet).
